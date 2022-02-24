6cad677dec6f3c654ee10a12304ec4b206ccd751
Melody McClure <melodylayne79@gmail.com>
2022-02-24 09:06:35 -0600

    partially functioning validation. The api call is still returning [object Object] and I cannot locate where I have not invoked correctly.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,92 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Form from './Components/Form'
+import Form from './Components/Form';
+import axios from 'axios';
+import schema from './validation/formSchema';
+import * as yup from 'yup';
 
 const initialFormValues = {
-  name: '',
-  email: '',
-  password: '',
-  tos: '',
-}
+    name: '',
+    email: '',
+    password: '',
+    tos: '',
+};
 
 const initialErrors = {
-  name: '',
-  email: '',
-  password: '',
-  tos: '',
-}
+    name: '',
+    email: '',
+    password: '',
+    tos: '',
+};
 
-const initialUsers = []
+const initialUsers = [];
 
 function App() {
-  const [users, setUsers] = useState(initialUsers)
-  const [formValues, setFormValues] = useState(initialFormValues)
-  const [errors, setErrors] = useState(initialErrors)
+    const [users, setUsers] = useState(initialUsers);
+    const [formValues, setFormValues] = useState(initialFormValues);
+    const [errors, setErrors] = useState(initialErrors);
 
-  const formSubmit = () => {
+    const getUsers = () => {
+        axios
+            .get(`https://reqres.in/api/users`)
+            .then((res) => {
+                console.log('first run' + res.data);
+                setUsers(res.data);
+                console.log(setUsers(res.data))
+            })
+            .catch((err) => console.error(err));
+    };
 
-  }
+    const postNewUser = (newUser) => {
+        axios
+            .post(`https://reqres.in/api/users`, newUser)
+            .then((res) => {
+                console.log(res.data + 'running');
+                setUsers([...users, res.data]);
+                setFormValues(initialFormValues);
+            })
+            .catch((err) => console.error(err));
+    };
 
+    const validate = (name, value) => {
+        yup.reach(schema, name)
+            .validate(value)
+            .then(() => setErrors({ ...errors, [name]: '' }))
+            .catch((err) => setErrors({ ...errors, [name]: err.errors[0] }));
+    };
 
-  return (
-    <div className="App">
-      <Form
-      values={formValues}
-      submit={formSubmit}
-      />
-    </div>
-  );
+    const inputChange = (name, value) => {
+      validate(name, value);
+      setFormValues({
+        ...formValues,
+        [name]: value
+      })
+    }
+
+    const formSubmit = () => {
+        const newUser = {
+            name: formValues.name.trim(),
+            email: formValues.email.trim(),
+            password: formValues.password.trim(),
+            tos: formValues.name,
+        };
+
+        postNewUser(newUser);
+    };
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    return (
+        <div className='App'>
+            <Form
+            values={formValues}
+            errors={errors}
+            submit={formSubmit}
+            change={inputChange}
+            />
+        </div>
+    );
 }
 export default App;
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,50 +2,83 @@ import React from 'react';
 import './form.css';
 
 export default function Form(props) {
-    const { name, values, submit } = props;
+    const {values, submit, change, errors } = props;
 
-    const onSubmit = (e) => {
+    const onSubmit = e => {
       e.preventDefault();
       submit()
     }
 
-    const onChange = (e) => {
-        const { name, email, password, tos } = e.target;
+    const onChange = e => {
+        const { name, value, type,  } = e.target;
+        change(name, value)
     };
 
     return (
         <form>
+            <div className='errors'>
+                <div>{errors.name}</div>
+                <div>{errors.email}</div>
+                <div>{errors.password}</div>
+                <div>{errors.terms}</div>
+            </div>
             Building stuff
-            <label> Please Enter a Name
-                <input name='name' type='text' placeholder='Hector Barbossa' value={values.name} onChange={onChange} />
-            </label>
-            <label> Please Enter an email address
-                <input name='email' type='email' value={values.email} placeholder='Yar1863@highseas.com' onChange={onChange} />
-            </label>
-            <label> Create a password
-                <input name='password' type='password' value={values.password} placeholder='Must be at least 4 characters long' onChange={onChange} />
-            </label>
-            <label id='radio'>I accept the Terms of Use
+            <label>
+
+                Please Enter a Name
                 <input
-                id='choice'
-                name='terms'
-                type='radio'
-                value='accept'
-                onChange={onChange}
-                checked={values.terms === 'accept'}
+                    value={values.name}
+                    onChange={onChange}
+                    name='name'
+                    type='text'
+                    placeholder='Hector Barbossa'
                 />
             </label>
-            <label id='radio'>I am disinclined to acquiesce
+            <label>
+
+                Please Enter an email address
+                <input
+                    name='email'
+                    type='email'
+                    value={values.email}
+                    placeholder='Yar1863@highseas.com'
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+
+                Create a password
+                <input
+                    name='password'
+                    type='password'
+                    value={values.password}
+                    placeholder='Must be at least 4 characters long'
+                    onChange={onChange}
+                />
+            </label>
+            <label id='radio'>
+                I accept the Terms of Use
                 <input
                     id='choice'
-                    name='terms'
+                    name='tos'
+                    type='radio'
+                    value='accept'
+                    onChange={onChange}
+                    checked={values.tos === 'I accept the Terms of Use'}
+                />
+            </label>
+            <label id='radio'>
+                I am disinclined to acquiesce
+                <input
+                    id='choice'
+                    name='tos'
                     type='radio'
                     value='decline'
                     onChange={onChange}
-                    checked={values.terms === 'decline'}
+                    checked={values.tos === 'I am disinclined to acquiesce'}
                 />
             </label>
-            <button type='submit' submit={submit}>Submit</button>
+            <button>Submit</button>
         </form>
     );
 }
